d359f7bf92ac14b2d24dda3a39f17e90f4146458
stevenngo3 <s1ngo@ucsd.edu>
2024-05-29 00:38:41 -0700

    Finish lab 8

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -49,7 +49,7 @@ function initializeServiceWorker() {
     window.addEventListener("load", (event) => {
       try {
         const registration = navigator.serviceWorker.register("./sw.js", {
-          scope: "/",
+          scope: "./",
         });
         if (registration.installing) {
           console.log("Service worker installing");
