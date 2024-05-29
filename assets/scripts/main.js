594ae096fa1f822e1da089345a0852d8e1adb275
stevenngo3 <s1ngo@ucsd.edu>
2024-05-29 02:41:15 -0700

    Finish lab 8

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -48,8 +48,8 @@ function initializeServiceWorker() {
   if ("serviceWorker" in navigator) {
     window.addEventListener("load", async () => {
       try {
-        const registration = await navigator.serviceWorker.register("./sw.js", {
-          scope: "./",
+        const registration = await navigator.serviceWorker.register("/sw.js", {
+          scope: "/",
         });
         if (registration.installing) {
           console.log("Service worker installing");
